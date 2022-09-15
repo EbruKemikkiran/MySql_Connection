034d8dd7ec7ea7c8e3f385184d28f4b2fb2e203c
Ebru Kemikkiran <104608342+EbruKemikkiran@users.noreply.github.com>
2022-09-15 14:54:22 +0100

    Add files via upload

---
+++ MySql/MySql/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace MySql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.txtDatabase = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.labelConnectionStatus = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lstProduct = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Database";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "User";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 16);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Password";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(118, 41);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(206, 22);
+            this.txtServer.TabIndex = 0;
+            // 
+            // txtDatabase
+            // 
+            this.txtDatabase.Location = new System.Drawing.Point(118, 79);
+            this.txtDatabase.Name = "txtDatabase";
+            this.txtDatabase.Size = new System.Drawing.Size(206, 22);
+            this.txtDatabase.TabIndex = 1;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(118, 117);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(206, 22);
+            this.txtUser.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(118, 155);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(206, 22);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // labelConnectionStatus
+            // 
+            this.labelConnectionStatus.AutoSize = true;
+            this.labelConnectionStatus.Location = new System.Drawing.Point(115, 201);
+            this.labelConnectionStatus.Name = "labelConnectionStatus";
+            this.labelConnectionStatus.Size = new System.Drawing.Size(72, 16);
+            this.labelConnectionStatus.TabIndex = 0;
+            this.labelConnectionStatus.Text = "Connected";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(156, 247);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lstProduct
+            // 
+            this.lstProduct.FormattingEnabled = true;
+            this.lstProduct.ItemHeight = 16;
+            this.lstProduct.Location = new System.Drawing.Point(370, 41);
+            this.lstProduct.Name = "lstProduct";
+            this.lstProduct.Size = new System.Drawing.Size(258, 132);
+            this.lstProduct.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstProduct);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtDatabase);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.labelConnectionStatus);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "MySqlC# Connection";
+            this.Shown += new System.EventHandler(this.btnConnect_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label labelConnectionStatus;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ListBox lstProduct;
+    }
+}
+
---
+++ MySql/MySql/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace MySql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            string _server =txtServer.Text;
+            string _database = txtDatabase.Text;
+            string _user = txtUser.Text;
+            string _password = txtPassword.Text;
+
+            string MySQLConnect = $"SERVER={_server}; DATABASE={_database}; UID={_user}; PWD={_password}";
+
+            if(string.IsNullOrEmpty(txtServer.Text)==true|| string.IsNullOrEmpty(txtDatabase.Text) == true || string.IsNullOrEmpty(txtUser.Text) == true || string.IsNullOrEmpty(txtPassword.Text) == true )
+            {
+                MessageBox.Show("Required", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else
+            {
+                using(var connect=new MySqlConnection(MySQLConnect))
+                {
+                    using (var cmd = new MySqlCommand("SELECT product FROM sales order by product ASC LIMIT 6", connect))
+                    {
+                        try
+                        {
+                            cmd.Connection.Open();
+                            MySqlDataReader mdr = cmd.ExecuteReader();
+                            lstProduct.Items.Add("Company Products");
+                            lstProduct.Items.Add("----------------");
+                            while (mdr.Read())
+                            {
+                                lstProduct.Items.Add(mdr[ "product"].ToString());
+                            }
+                            labelConnectionStatus.Text = "Connected";
+
+                        }
+                        catch (Exception error)
+                        {
+
+                            MessageBox.Show("Connection Failured:\n" + error.ToString(), "Error:" + error.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+
+    }
+}
